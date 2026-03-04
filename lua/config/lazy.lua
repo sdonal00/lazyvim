eb8848554b2f77b9447077020478110c03d359cb
Sam Donaldson <samdonaldson@corporatetools.com>
2026-03-04 16:18:04 -0600

    Changes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,8 +12,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     os.exit(1)
   end
 end
-vim.opt.rtp:prepend(lazypath)
 
+vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
@@ -30,7 +30,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "monokai_soda" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
