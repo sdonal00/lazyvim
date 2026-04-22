3b0630cbea6a9052474002c45f9dd3cd4dfee720
Sam <sdonal00@gmail.com>
2026-04-22 23:51:44 +0000

    Better autocomplete tab and enter

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -11,13 +11,21 @@ return {
         preset = "default",
 
         -- Tab confirms completion
-        ["<Tab>"] = { "select_and_accept" },
+       -- ["<Tab>"] = { "select_and_accept" },
 
         -- Shift-Tab moves backward
-        ["<S-Tab>"] = { "select_prev" },
+        --["<S-Tab>"] = { "select_prev" },
 
         -- Enter inserts newline (not accept)
-        ["<CR>"] = { "fallback" },
+        --["<CR>"] = { "fallback" },
+
+        -- Enter confirms completion
+        --["<CR>"] = { "accept", "fallback" },
+
+        ["<Tab>"]   = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+
+        ["<CR>"] = { "accept", "fallback" },      
       },
     },
   },
