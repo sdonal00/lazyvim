e2b7c9399a854f0a149b875adbb30b77c2ca7561
Sam <sdonal00@gmail.com>
2026-04-24 03:27:51 +0000

    leader W save

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>W", "<cmd>w<cr>", { desc = "Save file" })
+vim.api.nvim_create_user_command('W', 'w', { nargs = 0 })
+vim.api.nvim_create_user_command('Q', 'q', { nargs = 0 })
