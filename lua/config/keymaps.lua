b85eed7bbbba0beb5e08a24c390ea098268ef27e
sdonal00 <44108484+sdonal00@users.noreply.github.com>
2026-05-07 11:17:45 -0500

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,9 @@
 vim.keymap.set("n", "<leader>W", "<cmd>w<cr>", { desc = "Save file" })
 vim.api.nvim_create_user_command('W', 'w', { nargs = 0 })
 vim.api.nvim_create_user_command('Q', 'q', { nargs = 0 })
+
+vim.keymap.set("v", "<C-j>", ":move '>+1<CR>gv=gv", { desc = "Move lines down" })
+vim.keymap.set("v", "<C-k>", ":move '<-2<CR>gv=gv", { desc = "Move lines up" })
+
+vim.keymap.set("n", "<C-j>", ":move .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<C-k>", ":move .-2<CR>==", { desc = "Move line up" })
