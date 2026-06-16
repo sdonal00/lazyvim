bbf9e9555ffff471abcc9c17058ae43c2fc5b54e
Sam Donaldson <samdonaldson@corporatetools.com>
2026-06-16 13:14:55 -0500

    Style update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- vim.g.lazyvim_ruby_formatter = "standardrb"
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.breakindentopt = "shift:1"
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,56 +1,22 @@
---return {
---  {
---    "olimorris/onedarkpro.nvim",
---    -- "tanvirtin/monokai.nvim",
---    lazy = false,
---    priority = 1000,
---    config = function()
---      -- require("monokai").setup({ palette = require("monokai") })
---      require("onedarkpro").setup({})
---      vim.cmd("colorscheme onedark_dark")
---    end,
---  },
---}
---
 return {
   {
-    "olimorris/onedarkpro.nvim",
+    "navarasu/onedark.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      require("onedarkpro").setup({
-        colors = {
-          onedark_dark = {
-            bg        = "#000000",
-            fg        = "#abb2bf",
-            red       = "#f42340",
-            green     = "#5ddd44",
-            yellow    = "#f0b030",
-            blue      = "#2090f5",
-            purple    = "#cc22e8",
-            cyan      = "#0dccd8",
-            white     = "#abb2bf",
-            black     = "#434852",
-            orange    = "#f0b030",
-          }
-        },
-        -- highlights = {
-        --  ["@variable"]         = { fg = "${fg}" },
-        --  ["@keyword"]          = { fg = "${purple}" },
-        --  ["@function"]         = { fg = "${blue}" },
-       --   ["@string"]           = { fg = "${green}" },
-      --    ["@number"]           = { fg = "${orange}" },
-     --     ["@type"]             = { fg = "${yellow}" },
-    --      ["@comment"]          = { fg = "${black}" },
-   --       ["@constant"]         = { fg = "${cyan}" },
-  --        ["@parameter"]        = { fg = "${red}" },
- --       },
-        options = {
-          transparency = true,
-          cursorline = true,
-        }
+      require("onedark").setup({ 
+        style = "darker",
+	transparent = true,
+        ending_tildes = true,
+          code_style = {
+	    comments = 'none',
+	    keywords = 'none',
+	    functions = 'none',
+	    strings = 'none',
+	    variables = 'none'
+	 },
       })
-      vim.cmd("colorscheme onedark_dark")
+      require("onedark").load()
     end,
   },
 }
